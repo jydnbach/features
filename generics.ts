cbaeab1d2a63064b9bf609df8416f859c9a3d1c6
jydnbach <jaydenkim95@gmail.com>
2024-07-18 17:32:31 +0900

    more on generics

---
+++ generics.ts
@@ -0,0 +1,26 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+// two classes are similar
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+new ArrayOfAnything<string>(['a', 'b', 'c']);
+new ArrayOfAnything<number>([1, 2, 3]);
