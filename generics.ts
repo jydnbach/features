6990fb522194604ea76e650c7574a610ff65edf9
jydnbach <jaydenkim95@gmail.com>
2024-07-18 17:47:56 +0900

    type constraints

--- generics.ts
+++ generics.ts
@@ -51,3 +51,33 @@ function printAnything<T>(arr: T[]): void {
 printAnything<string>(['a', 'b', 'c']);
 // not <string[]> => that'd be string[][]
 // dont leave generic <> blank despite inference => help us catch errors
+
+// generic constraints
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+// type constraint: <T extends Printable>
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+// printHousesOrCars([1, 2, 3, 4]); // not allowed to pass in numbs bs of interface
+
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<Car>([new Car(), new Car()]);
