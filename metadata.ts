623ee2f5c48a15e0af2830a603c5e4a3d3a88030
jydnbach <jaydenkim95@gmail.com>
2024-07-26 18:26:30 +0900

    practical metadata

--- metadata.ts
+++ metadata.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red',
-};
+/////// metadta basics ////////
+// const plane = {
+//   color: 'red',
+// };
 
 // Reflect.defineMetadata('note', 'hi there', plane);
 // Reflect.defineMetadata('height', 10, plane);
@@ -15,8 +16,36 @@ const plane = {
 // console.log(note);
 // console.log(height);
 
-Reflect.defineMetadata('note', 'hi there', plane, 'color');
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
 
-const note = Reflect.getMetadata('note', plane, 'color');
+// const note = Reflect.getMetadata('note', plane, 'color');
 
-console.log(note);
+// console.log(note);
+
+/////////// practical metadata //////////
+
+@controller
+class Plane {
+  color: string = 'red';
+
+  @get('/login')
+  fly(): void {
+    console.log('vrrrr');
+  }
+}
+
+function get(path: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('path', path, target, key);
+  };
+}
+
+// typeof: constructor function of Plane
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const path = Reflect.getMetadata('path', target.prototype, key);
+    const middleware = Reflect.getMetadata('middleware', target.prototype, key);
+
+    router.get(path, middleware, target.prototype[key]);
+  }
+}
