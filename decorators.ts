bfb9bff47c803fc515db7c5178d5182be43c6743
jydnbach <jaydenkim95@gmail.com>
2024-07-26 16:44:19 +0900

    decorator factories

--- decorators.ts
+++ decorators.ts
@@ -8,26 +8,30 @@ class Boat {
   // decorator is syntatic sugar
   // @testDecorator
 
-  @logError
+  @logError('oops boat was sunk in ocean..')
   pilot(): void {
     throw new Error();
     console.log('swish');
   }
 }
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  const method = desc.value;
+// decorator factory
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
 
-  desc.value = function () {
-    try {
-      method();
-    } catch (e) {
-      console.log('oops, boat was sunk');
-    }
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+    // console.log('Target:', target);
+    // console.log('Key:', key);
   };
-  // console.log('Target:', target);
-  // console.log('Key:', key);
 }
+
 new Boat().pilot();
 
 // testDecorator(Boat.prototype, 'pilot');
